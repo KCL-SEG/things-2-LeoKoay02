0f7d50f8c04b365e1d6405aa46cc18c603e4638e
Leo Koay <k21087026@kcl.ac.uk>
2022-12-08 15:56:54 +0000

    '8/12'

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,12 @@
 """Forms of the project."""
-
+from django import forms
+from django.core.validators import RegexValidator
+from things.models import Thing
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widget = {'description': forms.Textarea(attrs={'maxlength': 120, 'required': True}),
+                  'quantity': forms.NumberInput()}
+        exclude = ['created_at']
